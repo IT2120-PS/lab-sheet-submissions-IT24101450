c8d22e2bf4242c4a3d5bbcda678db6deef009c16
IT24101450 <it24101450@my.sliit.lk>
2025-10-01 21:03:57 +0530

    Add files via upload

---
+++ Lab Sheet 09/Lab_9_Answers.R
@@ -0,0 +1,19 @@
+setwd('C:\\Users\\ASUS\\OneDrive\\Desktop\\PS_LAB9')
+
+##  Q1
+set.seed(42)               
+n <- 25
+sample <- rnorm(n, mean=45, sd=2)
+
+# View sample
+print(sample)
+
+##Q2
+# One-sample t-test: H0: mu = 46 vs H1: mu < 46 (left-tailed)
+t.res <- t.test(sample, mu = 46, alternative = "less")
+t.res
+
+
+t.res$statistic    # t statistic
+t.res$p.value      # p-value
+t.res$conf.int     # 95% CI for mean (two-sided)
